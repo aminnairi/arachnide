7fd18a5fbd6b8a52a3aac10066880a4676555657
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-04-16 15:27:42 +0200

    added the strip plugin configuration

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
+import strip from "@rollup/plugin-strip";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export default defineConfig({
   input: "src/index.ts",
   plugins: [
     typescript(),
-    process.env.NODE_ENV === "production" && terser()
+    isProduction && strip(),
+    isProduction && terser()
   ],
   output: {
     file: "dist/index.js",
