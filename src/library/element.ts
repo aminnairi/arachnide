942fddc700342691c32d9cd1e75796add7fbef2b
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-04-16 15:27:58 +0200

    more comments

--- src/library/element.ts
+++ src/library/element.ts
@@ -1,5 +1,8 @@
 import { VirtualObjectElement } from ".";
 
+/**
+ * Create a new virtual element based on a tag name, attributes and children. This function is mostly used whenever this library does not expose a function equivalent to a wanted HTML element
+ */
 export const element = (virtualObjectElement: VirtualObjectElement): VirtualObjectElement => {
   return virtualObjectElement;
 };
