46f911c76db33f18667ea33cf24702a876f8c6de
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-04-17 16:00:09 +0200

    fixed linter issues

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,7 +3,7 @@ import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import strip from "@rollup/plugin-strip";
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env["NODE_ENV"] === "production";
 
 export default defineConfig({
   input: "src/index.ts",
