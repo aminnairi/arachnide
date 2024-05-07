3201cd2143ea2a844377bb099c6be2b217d63e43
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-05-07 13:57:51 +0200

    added more functions for manipulating css properties

---
+++ packages/arachnide/css/src/properties/justifyContent.ts
@@ -0,0 +1,33 @@
+export type JustifyContentAlignment
+  = "center"
+  | "start"
+  | "end"
+  | "flex-start"
+  | "flex-end"
+  | "left"
+  | "right"
+  | "normal"
+  | "space-between"
+  | "space-around"
+  | "space-evenly"
+  | "stretch"
+
+export type JustifyContentOverflow
+  = `unsafe ${JustifyContentAlignment}`
+  | `safe ${JustifyContentAlignment}`
+
+export type JustifyContentGlobal
+  = "inherit"
+  | "initial"
+  | "revert"
+  | "revert-layer"
+  | "unset"
+
+export type JustifyContent
+  = JustifyContentAlignment
+  | JustifyContentOverflow
+  | JustifyContentGlobal
+
+export const justifyContent = (value: JustifyContent) => {
+  return `justify-content: ${value}`;
+};
