7c0e048f127c3ef389fbd993949b961531e32f10
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-05-07 11:28:44 +0200

    updated the example

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,7 +1,5 @@
 import { application } from "@arachnide/core";
-import { button, div, h1, input, p, table, tbody, td, tr } from "@arachnide/html";
-import { oninput } from "@arachnide/event";
-import { z } from "zod";
+import { div, button, span } from "@arachnide/html";
 
 const root = document.getElementById("root");
 
@@ -9,190 +7,66 @@ if (!root) {
   throw new Error("Root element not found");
 }
 
-const userSchema = z.object({
-  id: z.number(),
-  email: z.string().email()
-});
-
-const usersSchema = z.array(userSchema);
-
-type User = z.infer<typeof userSchema>;
-
 interface ApplicationState {
-  users: Array<User>,
-  loading: boolean,
-  error: null | string
+  counter: number
 }
 
 type ApplicationEvent
-  = { type: "SET_FETCHING", payload: null }
-  | { type: "SET_USERS", payload: Array<User> }
-  | { type: "SET_LOADING", payload: boolean }
-  | { type: "SET_ERROR", payload: string | null }
+  = { type: "INCREMENT", payload: null }
+  | { type: "DECREMENT", payload: null }
 
 application<ApplicationState, ApplicationEvent>({
   root,
   state: {
-    loading: false,
-    error: null,
-    users: []
+    counter: 15
   },
   update: ({ state, event }) => {
-    if (event.type === "SET_FETCHING") {
+    if (event.type === "INCREMENT") {
       return {
         ...state,
-        error: null,
-        loading: true
+        counter: state.counter + 1
       };
     }
 
-    if (event.type === "SET_USERS") {
+    if (event.type === "DECREMENT") {
       return {
         ...state,
-        users: event.payload
-      };
-    }
-
-    if (event.type === "SET_LOADING") {
-      return {
-        ...state,
-        loading: event.payload
-      };
-    }
-
-    if (event.type === "SET_ERROR") {
-      return {
-        ...state,
-        error: event.payload
+        counter: state.counter - 1
       };
     }
 
     return state;
   },
   view: ({ state, emit }) => {
-    const fetchUsers = async () => {
-      try {
-        emit({
-          type: "SET_FETCHING",
-          payload: null
-        });
-
-        const response = await fetch("https://jsonplaceholder.typicode.com/users");
-        const json = await response.json();
-        const users = usersSchema.parse(json);
-
-        emit({
-          type: "SET_USERS",
-          payload: users
-        });
-      } catch (error) {
-        emit({
-          type: "SET_ERROR",
-          payload: String(error)
-        });
-      } finally {
-        emit({
-          type: "SET_LOADING",
-          payload: false
-        });
-      }
-    };
-
-    if (state.loading) {
-      return div({
-        attributes: {},
-        children: [
-          h1({
-            attributes: {},
-            children: ["Loading"]
-          }),
-          p({
-            attributes: {},
-            children: ["Users list loading, please wait..."]
-          })
-        ]
-      });
-    }
-
-    if (state.error) {
-      return div({
-        attributes: {},
-        children: [
-          h1({
-            attributes: {},
-            children: ["Error"]
-          }),
-          p({
-            attributes: {},
-            children: ["An error occurred, please try again later"]
-          }),
-          button({
-            attributes: {
-              onclick: fetchUsers
-            },
-            children: ["Refresh"]
-          })
-        ]
-      });
-    }
-
-    if (state.users.length === 0) {
-      return div({
-        attributes: {},
-        children: [
-          p({
-            attributes: {},
-            children: ["No users in this list."]
-          }),
-          button({
-            attributes: {
-              onclick: fetchUsers
-            },
-            children: ["Refresh"]
-          })
-        ]
-      });
-    }
-
     return div({
       attributes: {},
       children: [
         button({
           attributes: {
-            onclick: fetchUsers
+            onclick: () => {
+              emit({
+                type: "DECREMENT",
+                payload: null
+              });
+            }
           },
-          children: ["Refresh"]
+          children: ["Decrement"]
         }),
-        input({
-          attributes: {
-            oninput: oninput((value) => {
-              console.log(value);
-            })
-          }
-        }),
-        table({
+        span({
           attributes: {},
-          children: [
-            tbody({
-              attributes: {},
-              children: state.users.map(user => {
-                return tr({
-                  attributes: {},
-                  children: [
-                    td({
-                      attributes: {},
-                      children: [user.id]
-                    }),
-                    td({
-                      attributes: {},
-                      children: [user.email]
-                    })
-                  ]
-                })
-              })
-            })
-          ]
-        })
+          children: [state.counter]
+        }),
+        button({
+          attributes: {
+            onclick: () => {
+              emit({
+                type: "INCREMENT",
+                payload: null
+              });
+            }
+          },
+          children: ["Increment"]
+        }),
       ]
     });
   }
