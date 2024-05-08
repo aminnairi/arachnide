12b393b9940136ae7e3bc8aa98ee5554e1fab2e5
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-05-08 21:57:57 +0200

    updated the live example

--- example/src/index.ts
+++ example/src/index.ts
@@ -37,37 +37,57 @@ application<ApplicationState, ApplicationEvent>({
 
     return state;
   },
-  view: ({ state, emit }) => {
-    return div({
-      attributes: {},
-      children: [
-        button({
-          attributes: {
-            onclick: () => {
-              emit({
-                type: "DECREMENT",
-                payload: null
-              });
-            }
-          },
-          children: ["Decrement"]
-        }),
-        span({
-          attributes: {},
-          children: [state.counter]
-        }),
-        button({
-          attributes: {
-            onclick: () => {
-              emit({
-                type: "INCREMENT",
-                payload: null
-              });
-            }
-          },
-          children: ["Increment"]
-        }),
-      ]
-    });
+  views: {
+    "/arachnide/users": ({ go }) => {
+      return button({
+        attributes: {
+          onclick: () => {
+            go("/arachnide", {})
+          }
+        },
+        children: ["Go back"]
+      });
+    },
+    "/arachnide": ({ state, emit, go }) => {
+      return div({
+        attributes: {},
+        children: [
+          button({
+            attributes: {
+              onclick: () => {
+                go("/arachnide/users", {})
+              }
+            },
+            children: ["Go to users"]
+          }),
+          button({
+            attributes: {
+              onclick: () => {
+                emit({
+                  type: "DECREMENT",
+                  payload: null
+                });
+              }
+            },
+            children: ["Decrement"]
+          }),
+          span({
+            attributes: {},
+            children: [state.counter]
+          }),
+          button({
+            attributes: {
+              onclick: () => {
+                emit({
+                  type: "INCREMENT",
+                  payload: null
+                });
+              }
+            },
+            children: ["Increment"]
+          }),
+        ]
+      });
+    }
   }
 });
