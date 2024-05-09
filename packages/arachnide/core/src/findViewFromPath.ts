74c178e15428d252f72a18ec9b61750139d49569
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-05-09 19:14:37 +0200

    updated the algorithm to account for parameterized routes

--- packages/arachnide/core/src/findViewFromPath.ts
+++ packages/arachnide/core/src/findViewFromPath.ts
@@ -1,16 +1,34 @@
 import { View } from "./types";
 import { removeTrailingLeadingSlashes } from "./removeTrailingLeadingSlashes";
 
-export const findViewFromPath = <ApplicationState, ApplicationEvent>(path: string, views: Record<string, View<ApplicationState, ApplicationEvent>>): View<ApplicationState, ApplicationEvent> => {
+export const findViewFromPath = <ApplicationState, ApplicationEvent>(path: string, views: Record<string, View<ApplicationState, ApplicationEvent>>): [string, View<ApplicationState, ApplicationEvent>] => {
   const viewsEntries = Object.entries(views);
 
   const foundViewEntry = viewsEntries.find(([viewPath]) => {
-    return removeTrailingLeadingSlashes(viewPath) === removeTrailingLeadingSlashes(path);
+    const normalizedViewPath = removeTrailingLeadingSlashes(viewPath);
+    const normalizedPath = removeTrailingLeadingSlashes(path);
+
+    const normalizedViewPathParts = normalizedViewPath.split(/\/+/);
+    const normalizedPathParts = normalizedPath.split(/\/+/);
+
+    if (normalizedPathParts.length !== normalizedViewPathParts.length) {
+      return false;
+    }
+
+    return normalizedViewPathParts.every((viewPathPart, index) => {
+      if (viewPathPart.startsWith(":")) {
+        return true;
+      }
+
+      const pathPart = normalizedPathParts[index];
+
+      return viewPathPart === pathPart;
+    });
   });
 
   if (foundViewEntry) {
-    return foundViewEntry[1];
+    return foundViewEntry;
   }
 
-  return () => null;
+  return ["", () => null];
 };
