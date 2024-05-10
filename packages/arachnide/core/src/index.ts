d361ce05de9143579e8878f40684db84eb49ea72
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-05-10 20:16:57 +0200

    exporting missing files from the source folder

--- packages/arachnide/core/src/index.ts
+++ packages/arachnide/core/src/index.ts
@@ -2,3 +2,7 @@ export * from "./types";
 export * from "./application";
 export * from "./createPatch";
 export * from "./element";
+export * from "./findViewFromPath";
+export * from "./getViewParameters"
+export * from "./removeTrailingLeadingSlashes";
+export * from "./render";
