b86dae91a881328c80f64c26557d8bdb54724eeb
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-05-15 21:53:26 +0200

    now preloading the picocss stylesheet

--- example/src/index.ts
+++ example/src/index.ts
@@ -215,3 +215,10 @@ startApplication<ApplicationState, ApplicationEvent>({
     }
   }
 });
+
+const picoStylesheet = document.createElement("link");
+
+picoStylesheet.href = "https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css";
+picoStylesheet.rel = "stylesheet";
+
+document.head.appendChild(picoStylesheet);
