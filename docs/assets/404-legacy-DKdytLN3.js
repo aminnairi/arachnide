10cab7bfcff6d19336232d3354f890957e252ff3
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-05-15 21:53:49 +0200

    updated the live example build files

---
+++ docs/assets/404-legacy-DKdytLN3.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-DR5yiC1C.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
--- docs/assets/404-oaVRe_xF.js
+++
@@ -1,1 +0,0 @@
-import"./index-UFDfdQ3d.js";function t(){import.meta.url,import("_").catch(()=>1),async function*(){}().next()}export{t as __vite_legacy_guard};
