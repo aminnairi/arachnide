f49dae03b5b3686452cf7ae6ad69f3382ce54e9b
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-05-16 10:34:15 +0200

    added more comments

--- packages/arachnide/core/src/isVirtualNullElement.ts
+++ packages/arachnide/core/src/isVirtualNullElement.ts
@@ -1,5 +1,11 @@
 import { VirtualElement } from "./types";
 
+/**
+ * Checks whether a data structure is null. This is really not a complicated
+ * operation, but is used in many location of the source-code of the framework
+ * so it becomes handy. You should not need this function, but you can use it
+ * for your algorithms if you really want to.
+ */
 export const isVirtualNullElement = (virtualElement: VirtualElement): virtualElement is null => {
   return virtualElement === null;
 };
